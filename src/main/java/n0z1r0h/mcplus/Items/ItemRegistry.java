d0d3784401208e62a509316c2bbc9f470ad77de4
N0z1r0h <ntge31@gmail.com>
2025-05-26 17:43:26 -0400

    Update ItemRegistry.java

--- src/main/java/n0z1r0h/mcplus/Items/ItemRegistry.java
+++ src/main/java/n0z1r0h/mcplus/Items/ItemRegistry.java
@@ -1,5 +1,23 @@
 package n0z1r0h.mcplus.Items;
 
+import n0z1r0h.mcplus.MinecraftPlus;
+import net.minecraft.item.Item;
+import net.minecraft.registry.Registries;
+import net.minecraft.registry.Registry;
+import net.minecraft.registry.RegistryKey;
+import net.minecraft.registry.RegistryKeys;
+import net.minecraft.util.Identifier;
+
+import java.util.function.Function;
+
 public class ItemRegistry {
-    
+    public static Item register(String name, Function<Item.Settings, Item> itemFactory, Item.Settings settings) {
+        RegistryKey<Item> itemKey = RegistryKey.of(RegistryKeys.ITEM, Identifier.of(MinecraftPlus.MOD_ID, name));
+
+        Item item = itemFactory.apply(settings.registryKey(itemKey));
+
+        Registry.register(Registries.ITEM, itemKey, item);
+        
+        return item;
+    }
 }
